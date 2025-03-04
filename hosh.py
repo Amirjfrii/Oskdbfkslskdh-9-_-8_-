8526d59a28209ee75d97b4ccd3b50af7c3adb555
Amirjfrii <131894835+Amirjfrii@users.noreply.github.com>
2025-03-04 16:07:27 +0330

    Create hosh.py

---
+++ hosh.py
@@ -0,0 +1,27 @@
+from telebot import TeleBot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReactionTypeEmoji
+import time
+import requests
+
+bot = TeleBot(token="2200135262:AAG8Ar9ag6GJPFq9QfRZ7QnvRL1cm9JbIhE/test")
+
+
+@bot.business_message_handler(func=lambda message: True)
+def echo_message(message):
+    api_url = f"https://api4dev.ir/ai/saveai/black.php?userid={message.chat.id}&Model=gpt-4o&text={message.text}"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        api_output = response.json().get('results')
+        bot.send_message(message.chat.id, api_output, business_connection_id=message.business_connection_id)
+    except requests.exceptions.RequestException as e:
+        bot.send_message(message.chat.id, f"Error: {e}", business_connection_id=message.business_connection_id)
+
+
+
+
+
+
+
+
+bot.polling()
