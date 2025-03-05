75196238c857f75f22ad19a26aea74e364f4a72d
Amirjfrii <131894835+Amirjfrii@users.noreply.github.com>
2025-03-05 15:45:57 +0330

    Update hosh.py

--- hosh.py
+++ hosh.py
@@ -8,12 +8,12 @@ bot = TeleBot(token="2200135262:AAG8Ar9ag6GJPFq9QfRZ7QnvRL1cm9JbIhE/test")
 
 @bot.business_message_handler(func=lambda message: True)
 def echo_message(message):
-    api_url = f"https://open.wiki-api.ir/apis-1/ChatGPT-4o?q={message.text}"
+    api_url = f"https://open.wiki-api.ir/apis-1/ChatGPT?q={message.text}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
         api_output = response.json().get('results')
-        bot.send_message(message.chat.id, api_output, business_connection_id=message.business_connection_id)
+        bot.send_message(message.chat.id, api_output, business_connection_id=message.business_connection_id, parse_mode=)
     except requests.exceptions.RequestException as e:
         bot.send_message(message.chat.id, f"Error: {e}", business_connection_id=message.business_connection_id)
 
