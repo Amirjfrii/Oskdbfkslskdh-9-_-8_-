065342b5d3f31e550ea3f7a451d7c5b8fd08feef
Amirjfrii <131894835+Amirjfrii@users.noreply.github.com>
2025-05-21 09:09:22 +0330

    Update hosh.py

--- hosh.py
+++ hosh.py
@@ -8,7 +8,7 @@ bot = TeleBot(token="2200135262:AAG5Bk9nK6N_qMcGhi5Q0gSyy3ClLFqZqDo/test")
 
 @bot.business_message_handler(func=lambda message: True)
 def echo_message(message):
-    api_url = f"http://backupapi.s6.viptelbot.top/advancedai/save?sender={message.chat.id}&text={message.text}"
+    api_url = f"https://api.pamickweb.ir/api/chat.php?apikey=u2aog60LOEX77965161495973057fxyl&text={m.text}&userid={m.chat.id}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
