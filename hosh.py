5a449cc7b7d3be1252eb4f0097dacb07893ad79b
Amirjfrii <131894835+Amirjfrii@users.noreply.github.com>
2025-05-21 09:18:29 +0330

    Update hosh.py

--- hosh.py
+++ hosh.py
@@ -8,7 +8,7 @@ bot = TeleBot(token="2200135262:AAHRzxyl09pbKwK7t1-KiRToZthzG8wuCNg/test")
 
 @bot.business_message_handler(func=lambda message: True)
 def echo_message(message):
-    api_url = f"https://api.pamickweb.ir/api/chat.php?apikey=u2aog60LOEX77965161495973057fxyl&text={m.text}&userid={m.chat.id}"
+    api_url = f"https://api.pamickweb.ir/api/chat.php?apikey=u2aog60LOEX77965161495973057fxyl&text={message.text}&userid={message.chat.id}"
     try:
         response = requests.get(api_url)
         response.raise_for_status()
